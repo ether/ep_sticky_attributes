bed7d04d199d4afbb9a70668c68ba0ac5ad9af2f
John McLear <john@mclear.co.uk>
2013-12-10 18:40:08 +0000

    toggle with buttons, still issues with getAttributes though..

--- static/js/sticky_attributes.js
+++ static/js/sticky_attributes.js
@@ -1,12 +1,32 @@
+var attributes = {
+  66: "bold",
+  73: "italic",
+  85: "underline"
+}
+
 exports.postAceInit = function(hook, context){
   $('#bold, #italic, #underline, #strikethrough').bind('click', function(button){
     var padeditor = require('ep_etherpad-lite/static/js/pad_editor').padeditor;
     return padeditor.ace.callWithAce(function (ace) {
       rep = ace.ace_getRep();
       if(rep.selStart[0] == rep.selEnd[0] && rep.selEnd[1] == rep.selStart[1]){
+        var attribute = $(button)[0].currentTarget.id;
         ace.ace_replaceRange(rep.selStart, rep.selEnd, "V");
-        ace.ace_performSelectionChange([rep.selStart[0],rep.selStart[1]-1], rep.selStart, false);
-        ace.ace_toggleAttributeOnSelection($(button)[0].currentTarget.id);
+        if(rep.selStart[1] != 1){ // seems messy but basically this is required to know if we're following a previous attribute
+          rep.selStart[1] = rep.selStart[1]-1;
+          rep.selEnd[1] = rep.selEnd[1]-1;
+          var isApplied = ace.ace_getAttributeOnSelection(attribute);
+          rep.selStart[1] = rep.selStart[1]+1;
+          rep.selEnd[1] = rep.selEnd[1]+1;
+        }
+        rep.selStart[1] = rep.selStart[1]-1; // overwrite the secret hidden character
+        if(!isApplied){ // If the attribute is not already applied
+          ace.ace_setAttributeOnSelection(attribute, true);
+          $('#'+attribute+' > a').addClass('activeButton');
+        }else{
+          ace.ace_setAttributeOnSelection(attribute, false);
+          $('#'+attribute+' > a').removeClass('activeButton');
+        }
         ace.ace_toggleAttributeOnSelection("hidden");
       }
     }, "stickyAttribute");
@@ -26,11 +46,6 @@ exports.aceKeyEvent = function(hook, callstack, editorInfo, rep, documentAttribu
   var k = evt.keyCode;
   var rep = callstack.rep;
   var documentAttributeManager = callstack.documentAttributeManager;
-  var attributes = {
-    66: "bold",
-    73: "italic",
-    85: "underline"
-  }
   if(rep.selStart[0] == rep.selEnd[0] && rep.selEnd[1] == rep.selStart[1]){
     if(evt.ctrlKey && (k == 66 || k == 73 || k == 85) && evt.type == "keyup"){
       // handling bold, italic or underline event
@@ -47,8 +62,10 @@ exports.aceKeyEvent = function(hook, callstack, editorInfo, rep, documentAttribu
       rep.selStart[1] = rep.selStart[1]-1; // overwrite the secret hidden character
       if(!isApplied){ // If the attribute is not already applied
         callstack.editorInfo.ace_setAttributeOnSelection(attribute, true);
+        $('#'+attribute+' > a').addClass('activeButton');
       }else{
         callstack.editorInfo.ace_setAttributeOnSelection(attribute, false);
+        $('#'+attribute+' > a').removeClass('activeButton');
       }
       documentAttributeManager.setAttributesOnRange(rep.selStart, rep.selEnd, [ ['hidden', true] ]); // hides the car
     }
@@ -60,24 +77,28 @@ exports.aceEditEvent = function(hook, call, editorInfo, rep, documentAttributeMa
   if(!(call.callstack.type == "handleClick") && !(call.callstack.type == "handleKeyEvent") && !(call.callstack.docTextChanged)){
     return false;
   }
+/*
+  if(!call.callstack.observedSelection){
+    console.log("Not observed");
+    return false;
+  }
+*/
   setTimeout(function(){ // avoid race condition..
-    // the caret is in a new position..  Let's do some funky shit
-    if ( call.editorInfo.ace_getAttributeOnSelection("bold") ) { // show the button as being depressed..  Not sad, but active.. You know the drill bitches.
-      $('#bold > a').addClass('activeButton');
-    }else{
-      $('#bold > a').removeClass('activeButton');
+    rep = call.rep;
+    if(rep.selStart[1] != 1){ // seems messy but basically this is required to know if we're following a previous attribute
+      $.each(attributes, function(k,attribute){
+        rep.selStart[1] = rep.selStart[1]-1;
+        rep.selEnd[1] = rep.selEnd[1]-1;
+        var isApplied = call.editorInfo.ace_getAttributeOnSelection(attribute);
+        if(isApplied){
+          $('#'+attribute+' > a').addClass('activeButton');
+        }else{
+          $('#'+attribute+' > a').removeClass('activeButton');
+        }
+        rep.selStart[1] = rep.selStart[1]+1;
+        rep.selEnd[1] = rep.selEnd[1]+1;
+      });
     }
-    if ( call.editorInfo.ace_getAttributeOnSelection("italic") ) { // show the button as being depressed..  Not sad, but active.. You know the drill bitches.
-      $('#italic > a').addClass('activeButton');
-    }else{
-      $('#italic > a').removeClass('activeButton');
-    }
-    if ( call.editorInfo.ace_getAttributeOnSelection("underline") ) { // show the button as being depressed..  Not sad, but active.. You know the drill bitches.
-      $('#underline > a').addClass('activeButton');
-    }else{
-      $('#underline > a').removeClass('activeButton');
-    }
-
   },250);
 }
 
