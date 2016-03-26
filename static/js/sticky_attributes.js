df3433bf44cc3ef4a166ab0d89b097378414222e
John McLear <john@mclear.co.uk>
2016-03-26 18:37:59 +0800

    rewrite to work properly

--- static/js/sticky_attributes.js
+++ static/js/sticky_attributes.js
@@ -19,18 +19,19 @@ exports.postAceInit = function(hook, context){
         var buttonEle = $(button)[0].currentTarget;
         var attribute = $(buttonEle).data("key");
 
-        // Replace the current
-        ace.ace_replaceRange(rep.selStart, rep.selEnd, "V");
-        if(rep.selStart[1] != 1){ // seems messy but basically this is required to know if we're following a previous attribute
-          rep.selStart[1] = rep.selStart[1]-1;
-          rep.selEnd[1] = rep.selEnd[1]-1;
-          var isApplied = ace.ace_getAttributeOnSelection(attribute);
-          rep.selStart[1] = rep.selStart[1]+1;
-          rep.selEnd[1] = rep.selEnd[1]+1;
+        if(rep.selStart[1] !== 1){ // seems messy but basically this is required to know if we're following a previous attribute
+          var isApplied = ace.ace_getAttributeOnSelection(attribute, true);
         }
+        var isFirstCharacter = (rep.selStart[1] === 0);
+        if(!isFirstCharacter) rep.selStart[1] = rep.selStart[1]+1;
+
+        // Append a hidden character the current caret position
+        ace.ace_replaceRange(rep.selStart, rep.selEnd, "V");
+
         rep.selStart[1] = rep.selStart[1]-1; // overwrite the secret hidden character
 
         if(!isApplied){ // If the attribute is not already applied
+          // console.log("enabling", attribute, "on selection");
           ace.ace_setAttributeOnSelection(attribute, true);
           $('.buttonicon-'+attribute).parent().addClass('activeButton');
         }else{
@@ -43,85 +44,110 @@ exports.postAceInit = function(hook, context){
   });
 };
 
-// Change the attribute into a class	
+// Change the attribute into a class
 exports.aceAttribsToClasses = function(hook, context){
   if(context.key.indexOf("hidden") !== -1){
     return ['hidden'];
   }
 }
 
-exports.aceKeyEvent = function(hook, callstack, editorInfo, rep, documentAttributeManager, evt){
+exports.aceKeyEvent = function(hook, callstack, cb){
   var evt = callstack.evt;
   var k = evt.keyCode;
-  var rep = callstack.rep;
   var documentAttributeManager = callstack.documentAttributeManager;
-  // If no text is selected..
-  if(rep.selStart[0] == rep.selEnd[0] && rep.selEnd[1] == rep.selStart[1]){
-    if(evt.ctrlKey && (k == 66 || k == 73 || k == 85 || k == 53) && evt.type == "keyup"){
-      // handling bold, italic or underline event
-      callstack.editorInfo.ace_replaceRange(undefined, undefined, "V"); // puts in a secret hidden cahracter
-      var attribute = attributes[k]; // which attribute is it?
-      // we need to know if the previous char has the attribute bold or not?
-      if(rep.selStart[1] != 1){ // seems messy but basically this is required to know if we're following a previous attribute
-        rep.selStart[1] = rep.selStart[1]-1;
-        rep.selEnd[1] = rep.selEnd[1]-1;
-        var isApplied = callstack.editorInfo.ace_getAttributeOnSelection(attribute);
-        rep.selStart[1] = rep.selStart[1]+1;
-        rep.selEnd[1] = rep.selEnd[1]+1;
-      }
-      rep.selStart[1] = rep.selStart[1]-1; // overwrite the secret hidden character
-      if(!isApplied){ // If the attribute is not already applied
-        callstack.editorInfo.ace_setAttributeOnSelection(attribute, true);
-        $('.buttonicon-'+attribute).parent().addClass('activeButton');
-      }else{
-        callstack.editorInfo.ace_setAttributeOnSelection(attribute, false);
-        $('.buttonicon-'+attribute).parent().removeClass('activeButton');
-      }
+  var isAttributeKey = (evt.ctrlKey && (k == 66 || k == 73 || k == 85 || k == 53) && evt.type === "keyup");
+  clientVars.sticky = {};
 
-      documentAttributeManager.setAttributesOnRange(rep.selStart, rep.selEnd, [ ['hidden', true] ]); // hides the car
-    }
-  }
-
-  // If text is selected IE Control B is placed when a selection is highlighted
-  if(rep.selStart[0] !== rep.selEnd[0] && rep.selEnd[1] !== rep.selStart[1]){
-    if(evt.ctrlKey && (k == 66 || k == 73 || k == 85) && evt.type == "keyup"){
-      checkAttr(callstack);
-    }
-  }
-}
-
-function checkAttr(call){
-  var rep = call.rep;
-  setTimeout(function(){ // avoid race condition..
-    if(rep.selStart[1] != 1){ // seems messy but basically this is required to know if we're following a previous attribute
-
-      if(rep.selStart[1] !== 0){
-        rep.selStart[1] = rep.selStart[1]-1;
-        rep.selEnd[1] = rep.selEnd[1]-1;
-      }
-      $.each(attributes, function(k,attribute){
-        var isApplied = call.editorInfo.ace_getAttributeOnSelection(attribute);
-        if(isApplied){
-          $('.buttonicon-'+attribute).parent().addClass('activeButton');
-        }else{
-          $('.buttonicon-'+attribute).parent().removeClass('activeButton');
-        }
-      });
-      if(rep.selStart[1] !== 0){
-        rep.selStart[1] = rep.selStart[1]+1;
-        rep.selEnd[1] = rep.selEnd[1]+1;
-      }
-
-    }
-  },250);
-}
-
-exports.aceEditEvent = function(hook, call, editorInfo, rep, documentAttributeManager){
-  // If it's not a click or a key event and the text hasn't changed then do nothing
-  if(!(call.callstack.type == "handleClick") && !(call.callstack.type == "handleKeyEvent") && !(call.callstack.docTextChanged)){
+  if(isAttributeKey){
+    var attribute = attributes[k]; // which attribute is it?
+    clientVars.sticky.setAttribute = true;
+    clientVars.sticky.attribute = attribute;
+  }else{
+    clientVars.sticky.setAttribute = false;
     return false;
   }
-  checkAttr(call);
+}
+
+function checkAttr(context, documentAttributeManager){
+  var rep = context.rep;
+  if(rep.selStart[1] != 1){ // seems messy but basically this is required to know if we're following a previous attribute
+    $.each(attributes, function(k,attribute){
+      var isApplied = documentAttributeManager.getAttributeOnSelection(attribute, true);
+      if(isApplied){
+        $('.buttonicon-'+attribute).parent().addClass('activeButton');
+      }else{
+        $('.buttonicon-'+attribute).parent().removeClass('activeButton');
+      }
+    });
+  }
+}
+
+
+exports.aceEditEvent = function(hook, context, editorInfo){
+  var call = context.callstack;
+  var documentAttributeManager = context.documentAttributeManager;
+  var padeditor = require('ep_etherpad-lite/static/js/pad_editor').padeditor;
+
+  if(call.type !== "idleWorkTimer") return;
+  var rep = context.documentAttributeManager.rep;
+  if(!rep.selStart && !rep.selEnd) return;
+
+  // Are we supposed to be applying or removing an attribute?
+  if(!clientVars.sticky || !clientVars.sticky.setAttribute){
+    var isToProcess = false;
+  }else{
+    var isToProcess = true;
+  }
+
+  if(isToProcess){
+    // Looks like we have work to do.. Let's go!
+    var isNotSelection = (rep.selStart[0] == rep.selEnd[0] && rep.selEnd[1] == rep.selStart[1]);
+    var hiddenCharacterSpan = (rep.selStart[1] === rep.selEnd[1]-1);
+    var hiddenCharacterSelected = documentAttributeManager.getAttributeOnSelection("hidden", false);
+    var isFirstCharacter = (rep.selStart[1] === 0);
+    var attribute = clientVars.sticky.attribute;
+
+    // console.log("isNotSelection", isNotSelection, "hiddenCharacterSpan", hiddenCharacterSpan, "hiddenCharSelected", hiddenCharacterSelected);
+    // console.log("isFirstCharacter", isFirstCharacter, "clientVars.sticky", clientVars.sticky);
+  }
+
+  // Create a hidden element and set the attribute on it
+  if(isNotSelection && isToProcess){
+    if(!isFirstCharacter) rep.selStart[1] = rep.selStart[1]-1;
+    var isApplied = documentAttributeManager.getAttributeOnSelection(attribute, false);
+    if(!isFirstCharacter) rep.selStart[1] = rep.selStart[1]+1;
+
+    // Create a hidden character
+    padeditor.ace.callWithAce(function (ace) {
+      ace.ace_replaceRange(undefined, undefined, "V"); // puts in a secret hidden cahracter
+    });
+
+    rep.selStart[1] = rep.selStart[1]-1; // overwrite the secret hidden character
+
+    if(!isApplied){ // If the attribute is not already applied
+      padeditor.ace.callWithAce(function (ace) {
+        ace.ace_setAttributeOnSelection(attribute, true);
+      });
+      $('.buttonicon-'+attribute).parent().addClass('activeButton');
+    }else{
+      padeditor.ace.callWithAce(function (ace) {
+        ace.ace_setAttributeOnSelection(attribute, false);
+      });
+      $('.buttonicon-'+attribute).parent().removeClass('activeButton');
+    }
+
+    // Set the hidden character to hidden
+    documentAttributeManager.setAttributesOnRange(rep.selStart, rep.selEnd, [ ['hidden', true] ]); // hides the char
+
+  }
+
+  if(clientVars.sticky){
+    clientVars.sticky.setAttribute = false;
+  }
+
+  setTimeout(function(){
+    checkAttr(context, documentAttributeManager);
+  }, 100);
 }
 
 exports.aceEditorCSS = function(hook_name, cb){return ["/ep_sticky_attributes/static/css/ace.css"];} // inner pad CSS
