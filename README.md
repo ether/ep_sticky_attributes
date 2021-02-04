a6e2a3a80188a6d1736b5e8665cafaad76da6534
John McLear <john@mclear.co.uk>
2021-02-04 12:21:58 +0000

    lint and readme

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
+# Make Bold et al sticky :) 
+Apply Bold/Italic/Underline with a click/control B keyboard command then type.
+This is useful if you are used to hitting control b and typing and expecting that text to be bold.
 
-# How can I bold/italic/underline upcoming text in Etherpad?
+## How it works
+![Screenshot](https://user-images.githubusercontent.com/220864/106892053-7450e500-66e3-11eb-9e08-c763d22f0628.gif)
 
-## Is there a way that one can bold upcoming text in Etherpad? For example, I want to press ctrl+b, and start typing in Bold. Then press ctrl+b again and go to normal formatting.
+## Installing
+npm install ep_sticky_attribiutes
 
-# Yes!
+or Use the Etherpad ``/admin`` interface.
 
-Use the Etherpad plugin installer to install the Sticky Attributes plugin
+## Testing
+No testing currently available for this plugin.  Please contribute some!
 
-Enjoy!  
+### Frontend
+
+Visit http://whatever/tests/frontend/ to run the frontend tests.
+
+### backend
+
+Type ``cd src && npm run test`` to run the backend tests.
+
+## LICENSE
+Apache 2.0
--- static/js/sticky_attributes.js
+++ static/js/sticky_attributes.js
@@ -1,60 +1,68 @@
-var attributes = {
+'use strict';
+
+const attributes = {
   66: 'bold',
   73: 'italic',
   85: 'underline',
   53: 'strikethrough',
 };
 
-exports.postAceInit = function (hook, context) {
+exports.postAceInit = (hook, context) => {
   // On click of a bold etc. button
-  $('.buttonicon-bold, .buttonicon-italic, .buttonicon-underline, .buttonicon-strikethrough').parent().parent().bind('click', (button) => {
-    const padeditor = require('ep_etherpad-lite/static/js/pad_editor').padeditor;
-    return padeditor.ace.callWithAce((ace) => {
-      rep = ace.ace_getRep();
+  $('.buttonicon-bold, .buttonicon-italic, .buttonicon-underline, .buttonicon-strikethrough')
+      .parent().parent().bind('click', (button) => {
+        const padeditor = require('ep_etherpad-lite/static/js/pad_editor').padeditor;
+        return padeditor.ace.callWithAce((ace) => {
+          const rep = ace.ace_getRep();
 
-      // if we're not selecting any text
-      if (rep.selStart[0] == rep.selEnd[0] && rep.selEnd[1] == rep.selStart[1]) {
-        // get the clicked attribute IE bold, italic
-        const buttonEle = $(button)[0].currentTarget;
-        const attribute = $(buttonEle).data('key');
+          // if we're not selecting any text
+          if (rep.selStart[0] === rep.selEnd[0] && rep.selEnd[1] === rep.selStart[1]) {
+            // get the clicked attribute IE bold, italic
+            const buttonEle = $(button)[0].currentTarget;
+            const attribute = $(buttonEle).data('key');
 
-        if (rep.selStart[1] !== 1) { // seems messy but basically this is required to know if we're following a previous attribute
-          var isApplied = ace.ace_getAttributeOnSelection(attribute, true);
-        }
-        const isFirstCharacter = (rep.selStart[1] === 0);
-        if (!isFirstCharacter) rep.selStart[1] = rep.selStart[1] + 1;
+            // seems messy but basically this is required to know if we're
+            // following a previous attribute
+            let isApplied;
+            if (rep.selStart[1] !== 1) {
+              isApplied = ace.ace_getAttributeOnSelection(attribute, true);
+            }
+            const isFirstCharacter = (rep.selStart[1] === 0);
+            if (!isFirstCharacter) rep.selStart[1] += 1;
 
-        // Append a hidden character the current caret position
-        ace.ace_replaceRange(rep.selStart, rep.selEnd, 'V');
+            // Append a hidden character the current caret position
+            ace.ace_replaceRange(rep.selStart, rep.selEnd, 'V');
 
-        rep.selStart[1] = rep.selStart[1] - 1; // overwrite the secret hidden character
+            rep.selStart[1] -= 1; // overwrite the secret hidden character
 
-        if (!isApplied) { // If the attribute is not already applied
-          // console.log("enabling", attribute, "on selection");
-          ace.ace_setAttributeOnSelection(attribute, true);
-          $(`.buttonicon-${attribute}`).parent().addClass('activeButton');
-        } else {
-          ace.ace_setAttributeOnSelection(attribute, false);
-          $(`.buttonicon-${attribute}`).parent().removeClass('activeButton');
-        }
-        ace.ace_toggleAttributeOnSelection('hidden');
-      }
-    }, 'stickyAttribute');
-  });
+            if (!isApplied) { // If the attribute is not already applied
+              // console.log("enabling", attribute, "on selection");
+              ace.ace_setAttributeOnSelection(attribute, true);
+              $(`.buttonicon-${attribute}`).parent().addClass('activeButton');
+            } else {
+              ace.ace_setAttributeOnSelection(attribute, false);
+              $(`.buttonicon-${attribute}`).parent().removeClass('activeButton');
+            }
+            ace.ace_toggleAttributeOnSelection('hidden');
+          }
+        }, 'stickyAttribute');
+      });
 };
 
 // Change the attribute into a class
-exports.aceAttribsToClasses = function (hook, context) {
+exports.aceAttribsToClasses = (hook, context) => {
   if (context.key.indexOf('hidden') !== -1) {
     return ['hidden'];
   }
 };
 
-exports.aceKeyEvent = function (hook, callstack, cb) {
+exports.aceKeyEvent = (hook, callstack, cb) => {
   const evt = callstack.evt;
   const k = evt.keyCode;
-  const documentAttributeManager = callstack.documentAttributeManager;
-  const isAttributeKey = (evt.ctrlKey && (k == 66 || k == 73 || k == 85 || k == 53) && evt.type === 'keyup');
+  const isAttributeKey = (
+    evt.ctrlKey && (
+      k === 66 || k === 73 || k === 85 || k === 53) && evt.type === 'keyup');
+
   clientVars.sticky = {};
 
   if (isAttributeKey) {
@@ -67,9 +75,11 @@ exports.aceKeyEvent = function (hook, callstack, cb) {
   }
 };
 
-function checkAttr(context, documentAttributeManager) {
+const checkAttr = (context, documentAttributeManager) => {
   const rep = context.rep;
-  if (rep.selStart[1] != 1) { // seems messy but basically this is required to know if we're following a previous attribute
+  // seems messy but basically this is required to know if
+  // we're following a previous attribute
+  if (rep.selStart[1] !== 1) {
     $.each(attributes, (k, attribute) => {
       const isApplied = documentAttributeManager.getAttributeOnSelection(attribute, true);
       if (isApplied) {
@@ -79,10 +89,10 @@ function checkAttr(context, documentAttributeManager) {
       }
     });
   }
-}
+};
 
 
-exports.aceEditEvent = function (hook, context, cb) {
+exports.aceEditEvent = (hook, context, cb) => {
   const call = context.callstack;
   const documentAttributeManager = context.documentAttributeManager;
   const padeditor = require('ep_etherpad-lite/static/js/pad_editor').padeditor;
@@ -92,36 +102,33 @@ exports.aceEditEvent = function (hook, context, cb) {
   if (!rep.selStart && !rep.selEnd) return cb();
 
   // Are we supposed to be applying or removing an attribute?
+  let isToProcess = true;
   if (!clientVars.sticky || !clientVars.sticky.setAttribute) {
-    var isToProcess = false;
-  } else {
-    var isToProcess = true;
+    isToProcess = false;
   }
+  let isNotSelection = false;
+  let isFirstCharacter;
+  let attribute;
 
   if (isToProcess) {
     // Looks like we have work to do.. Let's go!
-    var isNotSelection = (rep.selStart[0] == rep.selEnd[0] && rep.selEnd[1] == rep.selStart[1]);
-    const hiddenCharacterSpan = (rep.selStart[1] === rep.selEnd[1] - 1);
-    const hiddenCharacterSelected = documentAttributeManager.getAttributeOnSelection('hidden', false);
-    var isFirstCharacter = (rep.selStart[1] === 0);
-    var attribute = clientVars.sticky.attribute;
-
-    // console.log("isNotSelection", isNotSelection, "hiddenCharacterSpan", hiddenCharacterSpan, "hiddenCharSelected", hiddenCharacterSelected);
-    // console.log("isFirstCharacter", isFirstCharacter, "clientVars.sticky", clientVars.sticky);
+    isNotSelection = (rep.selStart[0] === rep.selEnd[0] && rep.selEnd[1] === rep.selStart[1]);
+    isFirstCharacter = (rep.selStart[1] === 0);
+    attribute = clientVars.sticky.attribute;
   }
 
   // Create a hidden element and set the attribute on it
   if (isNotSelection && isToProcess) {
-    if (!isFirstCharacter) rep.selStart[1] = rep.selStart[1] - 1;
+    if (!isFirstCharacter) rep.selStart[1] -= 1;
     const isApplied = documentAttributeManager.getAttributeOnSelection(attribute, false);
-    if (!isFirstCharacter) rep.selStart[1] = rep.selStart[1] + 1;
+    if (!isFirstCharacter) rep.selStart[1] += 1;
 
     // Create a hidden character
     padeditor.ace.callWithAce((ace) => {
       ace.ace_replaceRange(undefined, undefined, 'V'); // puts in a secret hidden cahracter
     });
 
-    rep.selStart[1] = rep.selStart[1] - 1; // overwrite the secret hidden character
+    rep.selStart[1] -= 1; // overwrite the secret hidden character
 
     if (!isApplied) { // If the attribute is not already applied
       padeditor.ace.callWithAce((ace) => {
@@ -136,7 +143,8 @@ exports.aceEditEvent = function (hook, context, cb) {
     }
 
     // Set the hidden character to hidden
-    documentAttributeManager.setAttributesOnRange(rep.selStart, rep.selEnd, [['hidden', true]]); // hides the char
+    documentAttributeManager.setAttributesOnRange(
+        rep.selStart, rep.selEnd, [['hidden', true]]); // hides the char
   }
 
   if (clientVars.sticky) {
@@ -149,4 +157,4 @@ exports.aceEditEvent = function (hook, context, cb) {
   return cb();
 };
 
-exports.aceEditorCSS = function (hook_name, cb) { return ['/ep_sticky_attributes/static/css/ace.css']; }; // inner pad CSS
+exports.aceEditorCSS = (hookName, cb) => ['/ep_sticky_attributes/static/css/ace.css'];
